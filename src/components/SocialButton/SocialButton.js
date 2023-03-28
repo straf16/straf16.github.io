c0c1240dec5d52dca836dcbd51b0e6ef9b6f28d8
ahmad.fadilah <ahmad.fadilah@bluebirdgroup.com>
2023-03-28 10:45:51 +0700

    update social button

--- src/components/SocialButton/SocialButton.js
+++ src/components/SocialButton/SocialButton.js
@@ -6,8 +6,8 @@ import linkedinIcon from '../../assets/linkedin.png'
 const SocialButton = () => {
   return (
     <div className='icon-wrapper'>
-      <img id="github-icon" src={githubIcon} height={25} width={25} alt="github-icon" />
-      <img id="linkedin-icon" src={linkedinIcon} height={25} width={25} alt="linkedin-icon" />
+      <img id="github-icon" className="social-icon" src={githubIcon} height={25} width={25} alt="github-icon" />
+      <img id="linkedin-icon" className="social-icon" src={linkedinIcon} height={25} width={25} alt="linkedin-icon" />
     </div>
   )
 }
